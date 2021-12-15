130fd0e3e9f913e99b7160f68c9607cbec255302
VinhNguyen <thevinh13.it@gmail.com>
2021-12-15 18:33:20 +0700

    update scroll

--- js/app.js
+++ js/app.js
@@ -1,91 +1,71 @@
 /*
-   Author: Cyforce
+   Author: Return
    Version: 1.0.0
    Created: Dec 2021
    File Description: Main js file
 */
 
 (function ($) {
+	'use strict';
 
-    'use strict';
+	//Loader
+	$(window).on('load', function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(350).fadeOut('slow');
+		$('body').delay(350).css({
+			'overflow': 'visible'
+		});
+	});
+
+
 	// STICKY
 	$(window).scroll(function() {
-		onScrollHandle();
+		let scroll = $(window).scrollTop();
+		$(".navbar").toggleClass("nav-sticky", scroll > 50);
 	});
 
-	// SmoothLink
-	$('.nav-item a, .mouse-down a').on('click', function(event) {
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top - 0
-		}, 1000, 'easeInOutExpo');
-		event.preventDefault();
-	});
-
-	// scrollspy
-	$(".navbar-nav").scrollspy({
-		offset: 70
-	});
-
-
-	//owlCarousel
+	//OwlCarousel
 	$('.owl-carousel').owlCarousel({
-	    autoplay:true,
-	    autoplayTimeout:3000,
-	    loop:true,
-	    margin:10,
-	    nav:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:2
-	        },
-	        1000:{
-	            items:3
-	        }
-	    }
+		autoplay:true,
+		autoplayTimeout:3000,
+		loop:true,
+		margin:10,
+		nav:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
 	});
 
-	// Loader
-	$(window).on('load', function() {
-	    $('#status').fadeOut();
-	    $('#preloader').delay(350).fadeOut('slow');
-	    $('body').delay(350).css({
-	        'overflow': 'visible'
-	    });
-	});
-
-	// Scroll one page
+	//Scroll
 	$.scrollify({
-		section : "section",
-		sectionName : "section-name",
-		interstitialSection:".header,.footer",
+		section:".section",
+		scrollbars:false,
+		interstitialSection: ".header,.footer",
 		easing: "easeOutExpo",
 		scrollSpeed: 1000,
-		offset : 0,
-		scrollbars: false,
-		standardScrollElements: "",
+		offset: 0,
 		setHeights: true,
 		overflowScroll: true,
 		updateHash: false,
-		touchScroll:true
+		touchScroll: true,
+		before:function(i,panels) {
+			let ref = panels[i].attr("data-section-name");
+			$(".navbar .navbar-nav .nav-item.active").removeClass("active");
+			$(".navbar-nav .nav-item").find("a[href=\"#" + ref + "\"]").closest(".nav-item").addClass("active");
+		},
+		afterRender:function() {
+			$(".nav-item a").on("click",function(e) {
+				$.scrollify.move($(this).attr("href"));
+				e.preventDefault();
+			});
+		}
 	});
-
-	function onScrollHandle() {
-		var scroll = $(window).scrollTop();
-		$(".navbar").toggleClass("nav-sticky", scroll > 50);
-
-		$('#navbarCollapse > ul > li > a').each(function () {
-			var curLink = $(this);
-			var refElem = $(curLink.attr('href'));
-			if (refElem.position().top <= scroll && refElem.position().top + refElem.height() > scroll) {
-				$('#navbarCollapse > ul > li').removeClass("active");
-				curLink.parent().addClass("active");
-			} else {
-				curLink.parent().removeClass("active");
-			}
-		});
-	}
-})(jQuery)
+})(jQuery);
